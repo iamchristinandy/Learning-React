6424d209b9c831168eba5500ebc99951ff2ad4ca
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-06-26 15:48:13 +0000

    Managing checkboxes using useState

--- Exercise Files/Ch04/04_02/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_02/start/react-app/src/App.js
@@ -7,33 +7,17 @@ import { useState, useEffect } from 'react';
 // console.log(second);
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondaryEmotion] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-    }, [emotion]);
-
-    useEffect(() => {
-      console.log(`It's ${secondary}`)
-    });
-  return (
-    <div className="App">
-      <h1>Curent emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>
-        Sad
-        </button>
-        <button onClick={() => setEmotion("Awful")}>
-        awful
-        </button>
-        <button
-        onClick={() => setEmotion("Excited")}
-        >
-          Excited
-        </button>
-        <h2>Current secondary emotion is {secondary}.</h2>
-        <button onClick={()=>setSecondaryEmotion("grateful")}>Grateful</button>
-
+  const [checked, setChecked] = useState(false)
+    return (
+      <div className="App">
+          <input type="checkbox"
+          value={checked}
+          onChange={()=>
+          setChecked((checked) => !checked)
+          }
+          />
+          <label>{checked ? "checked": "not checked"}
+          </label>
     </div>
   );
 }
