25a19be58674d7ee4046c2425fd83bf9edcd9272
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-06-27 16:15:28 +0000

    using useRef in react to get UI components

--- Exercise Files/Ch04/04_02/start/react-app/src/App.js
+++ Exercise Files/Ch04/04_02/start/react-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect, useReducer } from 'react';
+import { useRef } from 'react';
 
 
 // const [firstcity, second] = ["Accra", "Tamale", "Nsawam", "Kumasi"];
@@ -7,19 +7,40 @@ import { useState, useEffect, useReducer } from 'react';
 // console.log(second);
 
 function App() {
-  const [checked, setChecked] = useReducer (checked => !setChecked, false);
-    return (
-      <div className="App">
-          <input type="checkbox"
-          value={checked}
-          onChange={(setChecked)=>
-          setChecked((checked) => !checked)
-          }
-          />
-          <label>{checked ? "checked": "not checked"}
-          </label>
-    </div>
-  );
-}
+            const txtTitle = useRef();
+            const hexColor = useRef();
+            const submit = e => {
+              e.preventDefault();
+              const title = txtTitle.current.value;
+              const color = hexColor.current.value;
+              alert(`${title}, ${color}`);
+              txtTitle.current.value = "";
+              hexColor.current.value = "";
+            }
+        return (
+            <form onSubmit={submit}>
+                <input
+                ref={txtTitle}
+                type='text'
+                placeholder='colour title...'/>
+                <input ref={hexColor}
+                type="color"/>
+                <button>Add</button>
+            </form>
+        )
+  // const [checked, setChecked] = useReducer (checked => !setChecked, false);
+  //   return (
+  //     <div className="App">
+  //         <input type="checkbox"
+  //         value={checked}
+  //         onChange={(setChecked)=>
+  //         setChecked((checked) => !checked)
+  //         }
+  //         />
+  //         <label>{checked ? "checked": "not checked"}
+  //         </label>
+  //  </div>
+  
+};
 
 export default App;
